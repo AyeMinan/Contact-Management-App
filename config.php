3fbb6f30de1f74f493c7f8b0a9f32ea4d5e48bf6
AyeMinan <conqueror768@gmail.com>
2024-05-02 22:39:35 +0630

    table creation added

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
+
 <?php
 
 define("HOSTNAME", "localhost");
@@ -6,7 +7,17 @@ define("PASSWORD", "");
 define("DATABASE", "contact_management_app");
 
 $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+$sql = "CREATE TABLE `contact_info` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+   ` email` VARCHAR(100) NOT NULL unique,
+    `phone_number` VARCHAR(20) NOT NULL,
+    `address` TEXT
+)";
 
-if (!$connection) {
-    die("Connection Failed");
+$result = mysqli_query($connection, $sql);
+
+if (!$result) {
+    die("Table Creation Failed");
 }
