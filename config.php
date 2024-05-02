b96f1624c5ac4e23b3756d687b578ce17467806c
AyeMinan <conqueror768@gmail.com>
2024-05-02 23:08:01 +0630

    table creation changes

--- config.php
+++ config.php
@@ -7,17 +7,19 @@ define("PASSWORD", "");
 define("DATABASE", "contact_management_app");
 
 $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
-$sql = "CREATE TABLE `contact_info` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `first_name` VARCHAR(50) NOT NULL,
-    `last_name` VARCHAR(50) NOT NULL,
-   ` email` VARCHAR(100) NOT NULL unique,
-    `phone_number` VARCHAR(20) NOT NULL,
-    `address` TEXT
-)";
+$sqlCheck = "SHOW TABLES LIKE 'contact_info'";
+$resultCheck = mysqli_query($connection, $sqlCheck);
 
-$result = mysqli_query($connection, $sql);
+if (mysqli_num_rows($resultCheck) == 0) {
+    // Table does not exist, so table will be created
+    $sqlCreate = "CREATE TABLE `contact_info` (
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
+        `first_name` VARCHAR(50) NOT NULL,
+        `last_name` VARCHAR(50) NOT NULL,
+        `email` VARCHAR(100) NOT NULL unique,
+        `phone_number` VARCHAR(20) NOT NULL,
+        `address` TEXT
+    )";
+    $result = mysqli_query($connection, $sqlCreate);
+} 
 
-if (!$result) {
-    die("Table Creation Failed");
-}
